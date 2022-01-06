022ebf950021ec8784e6e9e87e8ee278e8127f9c
Alexander Belanger <belanger@sas.upenn.edu>
2022-01-06 07:54:46 -0500

    revert to v0.13.7 for testing

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -1,13 +1,13 @@
 class Porter < Formula
-  homepage https://porter.run
-  version v0.14.0-testing8
+  homepage "https://porter.run"
+  version "v0.13.7"
 
   on_macos do
-    url https://github.com/porter-dev/porter/releases/download/v0.14.0-testing8/porter_v0.14.0-testing8_Darwin_x86_64.zip
-    sha256 testing
+    url "https://github.com/porter-dev/porter/releases/download/v0.13.7/porter_v0.13.7_Darwin_x86_64.zip"
+    sha256 "c9b3bbb17493d9922177919d53b14203e9580fccb73252bccf07f1fe384b4bbe"
 
     def install
-      bin.install porter
+      bin.install "porter"
     end
   end
 end
