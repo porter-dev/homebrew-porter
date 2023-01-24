525fa9ebf18060bd07c51e37c3597ac4a6d26da1
Stefan McShane <stefanmcshane@users.noreply.github.com>
2023-01-24 13:24:41 -0500

    Update porter.rb

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -4,7 +4,7 @@ class Porter < Formula
 
   on_macos do
     url "https://github.com/porter-dev/porter/releases/download/v0.42.5/porter_v0.42.5_Darwin_x86_64.zip"
-    sha256 "SHA256b21fb1be0326b9c7128aea37bfe9be17f0cb0fe9a5001a055fd0dec812965f80"
+    sha256 "b21fb1be0326b9c7128aea37bfe9be17f0cb0fe9a5001a055fd0dec812965f80"
 
     def install
       bin.install "porter"
