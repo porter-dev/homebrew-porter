f1a2fce2ed34c137564d0ceb6abc93ec2880a816
ianedwards <ianedwards559@gmail.com>
2024-03-18 18:30:10 -0400

    Update porter.rb

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -1,9 +1,9 @@
 class Porter < Formula
   homepage "https://porter.run"
-  version "v0.52.44"
+  version "v0.52.43"
 
   on_macos do
-    url "https://github.com/porter-dev/porter/releases/download/v0.52.44/porter_v0.52.44_Darwin_x86_64.zip"
+    url "https://github.com/porter-dev/porter/releases/download/v0.52.43/porter_v0.52.43_Darwin_x86_64.zip"
     sha256 "c43825ece8ec28d8959c2220a2511a8f57fa9c119d484d09eb7dd299c73c6c12"
 
     def install
