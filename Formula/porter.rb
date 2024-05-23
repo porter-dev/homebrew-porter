8f2a6d806bc01dadac0e98026dafa0566a05813c
Philip Arndt <git@p.arndt.io>
2024-05-23 12:43:48 +1200

    Update repo address
    It seems like the repository hosting these releases has changed.

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -3,7 +3,7 @@ class Porter < Formula
   version "v0.52.55"
 
   on_macos do
-    url "https://github.com/porter-dev/porter/releases/download/v0.52.55/porter_v0.52.55_Darwin_x86_64.zip"
+    url "https://github.com/porter-dev/porter-archive/releases/download/v0.52.55/porter_v0.52.55_Darwin_x86_64.zip"
     sha256 "5f5287f479253d98b3661a90f30d19f614be6af416dfcd20ed9dae028253d5fc"
 
     def install
