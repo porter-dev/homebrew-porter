2c6f49af9393ecba6614e1c8b9e9619482d8a98d
Mauricio Araujo <mauricio.araujo@proton.me>
2024-05-23 12:03:02 -0400

    Change repo

--- Formula/porter.rb
+++ Formula/porter.rb
@@ -3,7 +3,7 @@ class Porter < Formula
   version "v0.52.56"
 
   on_macos do
-    url "https://github.com/porter-dev/porter/releases/download/v0.52.56/porter_v0.52.56_Darwin_x86_64.zip"
+    url "https://github.com/porter-dev/porter-archive/releases/download/v0.52.56/porter_v0.52.56_Darwin_x86_64.zip"
     sha256 "cc6f7fdc3edd398cf0e052f6f452076c9904877eac13a3f0ccc147700fc28a86"
 
     def install
